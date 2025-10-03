328acba7476f38eb6c2bfe8f16ff804b68de8c3b
Enrico Marquez <enricovmarquezz@gmail.com>
2025-10-03 18:54:19 -0300

    feat: define `entitys` and configure `migrations`

---
+++ src/entitys/session.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .base import Base
+
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    token: Mapped[str] = mapped_column(String(256))
+    expires_at: Mapped[datetime]
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user: Mapped["User"] = relationship(back_populates="sessions")
